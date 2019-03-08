ee5265a624f9de0e2c1a24bae57c96d07707e26c
Reberth da Silva <reberth.silva@kroton.com.br>
2019-03-08 00:19:08 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db9bf58a3d3d4f18c3e275cce2e31785.js"
+  "/reberthdasilva.github.io/precache-manifest.a22d27fdb09b9f8c876153cbc191fd8e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reberthdasilva.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
